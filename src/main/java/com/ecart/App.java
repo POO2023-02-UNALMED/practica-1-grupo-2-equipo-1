1cb4934187caa9c2e536f6f9bac3daa2e1a358f5
secadavida <secadavida@unal.edu.co>
2023-10-05 22:27:07 -0500

    feat: classes

--- src/main/java/com/ecart/App.java
+++ src/main/java/com/ecart/App.java
@@ -1,13 +1,11 @@
 package com.ecart;
 
 /**
- * Hello world!
+ * App's entry point
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
