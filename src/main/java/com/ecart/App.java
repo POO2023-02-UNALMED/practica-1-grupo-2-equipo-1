532c174cd83cba479b335950176bbee9c92bb982
secadavida <secadavida@unal.edu.co>
2023-10-20 00:35:08 -0500

    chore(app): deactivated system console check

--- src/main/java/com/ecart/App.java
+++ src/main/java/com/ecart/App.java
@@ -6,15 +6,15 @@ import static com.ecart.uiMain.Utils.clear;
 /** App's entry point */
 public class App {
 	public static void main(String[] args) {
-		if (System.console() != null) {
+		// if (System.console() != null) {
 			// executed before the main program exists (forcefully)
 			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 				shutdown(false);
 			}));
 
 			menu();
-		} else
-			System.err.println("The system is not running interactively");
+		// } else
+		// 	System.err.println("The system is not running interactively");
 	}
 
 	public static void shutdown(boolean force) {
