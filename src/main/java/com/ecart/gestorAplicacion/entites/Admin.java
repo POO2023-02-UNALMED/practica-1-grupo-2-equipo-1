673abce201fcdad1f44aed402177be77307c02ba
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-10-22 17:09:32 -0500

    Add validate|create

--- src/main/java/com/ecart/gestorAplicacion/entites/Admin.java
+++ src/main/java/com/ecart/gestorAplicacion/entites/Admin.java
@@ -4,11 +4,53 @@ import java.util.ArrayList;
 
 public class Admin extends Person {
 	private static ArrayList<Admin> instances = new ArrayList<>();
+	private  ArrayList<Delivery> deliveries = new ArrayList<>();
+
 
 	public Admin(String username, String password, int[] address) {
 		super(username, password, address);
 
 		instances.add(this);
 	}
+
+	public static Admin validate(String username) {
+		return validate(username, null, instances, false);
+	}
+
+	public static Admin validate(String username, String password) {
+		return validate(username, password, instances, true);
+	}
+
+	public static Admin validate(String username, ArrayList<Admin> arr) {
+
+		return validate(username, null, arr, false);
+	}
+
+	public static Admin validate(String username, String password, ArrayList<Admin> arr, boolean checkPassword) {
+		for (Admin admin : arr) {
+			if (admin.getName().equals(username)) {
+				if (checkPassword) {
+					if (!admin.getPassword().equals(password))
+						continue;
+				}
+
+				return admin;
+			}
+		}
+		return null;
+	}
+
+	public static Admin create(String name, String password, int[] address) {
+		Admin newAdmin = validate(name);
+		if (newAdmin != null)
+			return null;
+
+		if(!Person.isAddressAvailable(address))
+			return null;
+
+		newAdmin = new Admin(name, password, address);
+		return newAdmin;
+	}
+
 }
 
